2fbe6e075f0145183ab6212d4e8b6c98110d52bd
sohag-arnifi <sohag.b@arnifi.com>
2024-03-20 05:17:28 +0600

    pdf generate initial function added

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,6 +2,22 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const scrapeLogic = async (res) => {
+  const html = `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>test pdf</title>
+  </head>
+  <body>
+    this is test html pdf
+  </body>
+</html>
+    `;
+
+  //   const browser = await puppeteer.launch();
+
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
@@ -14,37 +30,23 @@ const scrapeLogic = async (res) => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+
   try {
     const page = await browser.newPage();
+    await page.setContent(html);
 
-    await page.goto("https://developer.chrome.com/");
+    const pdf = await page.pdf({
+      format: "a4",
+      printBackground: true,
+    });
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+    console.log(pdf);
+    await browser.close();
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    res.send("ok");
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
   }
 };
 
