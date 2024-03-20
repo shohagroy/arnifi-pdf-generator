aa9524b97abe59d3119efe4cc322741dc9e74bbf
sohag-arnifi <sohag.b@arnifi.com>
2024-03-20 06:33:47 +0600

    code updated

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,52 +1,26 @@
 require("dotenv").config();
-
-let chrome = {};
-let puppeteer;
-
-//puppeteer-core@6.0.0
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  console.log("running on lambda");
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  console.log("running on local");
-  puppeteer = require("puppeteer");
-}
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 const scrapeLogic = async (res) => {
-  console.log("scrapeLogic run");
-  const html = `
-    <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>test pdf</title>
-  </head>
-  <body>
-    this is test html pdf
-  </body>
-</html>
-    `;
-
-  let options = {};
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    console.log("running on lambda");
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+  let options = {
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : await chrome.executablePath(),
+    headless: true,
+    ignoreHTTPSErrors: true,
+  };
 
   try {
     console.log("launching puppeteer");
     let browser = await puppeteer.launch(options);
-
     console.log("browser created");
     const page = await browser.newPage();
 
@@ -59,9 +33,6 @@ const scrapeLogic = async (res) => {
     });
 
     console.log("pdf created");
-
-    // let page = await browser.newPage();
-    // await page.goto("https://www.google.com");
 
     res.json({ message: "pdf create", pdf });
   } catch (err) {
